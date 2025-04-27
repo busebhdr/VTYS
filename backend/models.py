a840449de25a3011cdae921fb23c100d352ba669
busebhdr <167623284+busebhdr@users.noreply.github.com>
2025-04-27 21:34:48 +0300

    Backend ve frontend klasörleri oluşturuldu, dosyalar eklendi

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    todos = relationship("ToDo", back_populates="owner")
+
+class ToDo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="todos")
